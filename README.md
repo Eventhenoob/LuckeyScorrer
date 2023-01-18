7597dddd4a678f55865374a2bb3d07d1f4a70113
Bharat gawar <106008597+Eventhenoob@users.noreply.github.com>
2023-01-18 19:26:11 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# Lucky Scorrer
+
+Lucky Scorrer is Multiplayer Game which can be played by two 
+people at once. first both player's have to choose there name's 
+and after this the player-1 get the first chance and he either
+can roll the dice and increase his/her current score or choose to
+hold the current score that after pressing hold will be added to
+ his/her main score and the current score will set to 0 and then 
+ the player-2 will get the chance to do the same.
+
+ **Rules** 
+ - if player on rolling the dice get the 1 as the number from the dice then his/her current score the he/she has scorred so far will be reset to 0 and game will switch to other player for there chance.
+
+ **how to win**
+ - The first player to score 100 as main score will win the game.
+## Technologies Used
+
+**Technologies:** HTML, Css, JavaScript
+## Color Reference
+
+| Color             | Hex                                                                |
+| ----------------- | ------------------------------------------------------------------ |
+| Color Primary Before Winning  | ![#edcc0f](https://via.placeholder.com/10/edcc0f?text=+) #edcc0f |
+| Color Secondary Before Winning | ![#7abf0ac3](https://via.placeholder.com/10/7abf0ac3?text=+) #7abf0ac3 |
+| Color Primary After Winning | ![#DF2935](https://via.placeholder.com/10/DF2935?text=+) #DF2935 |
+| Color Secondary After Winning | ![#C94E4C](https://via.placeholder.com/10/C94E4C?text=+) #C94E4C |
+| Color Active Side Before Winning | ![#dcdcdcaa](https://via.placeholder.com/10/dcdcdcaa?text=+) #dcdcdcaa |
+| Color Active Side After Winning | ![#222222](https://via.placeholder.com/10/222222?text=+) #222222 |
+
+## Authors
+
+- [@Eventhenoob](https://github.com/Eventhenoob)
+
---
+++ luckey scorrer/script.js
@@ -0,0 +1,134 @@
+"use strict";
+// naming players
+const player1Name = prompt("Enter First Player Name:");
+const player2Name = prompt("Enter Second Player Name:");
+document.querySelector(".player--1").textContent = player1Name;
+document.querySelector(".player--2").textContent = player2Name;
+
+// User Interface
+
+var root = document.querySelector(":root");
+
+const side1UI = document.querySelector(".side-1");
+const side2UI = document.querySelector(".side-2");
+
+const scoreMain1UI = document.querySelector(".score--main-1");
+const scoreMain2UI = document.querySelector(".score--main-2");
+
+const currentScore1UI = document.querySelector(".score--sub-1");
+const currentScore2UI = document.querySelector(".score--sub-2");
+
+const diceUI = document.querySelector(".dice");
+
+// Button's
+const btnReset = document.querySelector(".btn--reset");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+// Mechanics
+let currentPlayer = 1;
+let currentScore = 0;
+
+let mainScore1 = 0;
+let mainScore2 = 0;
+
+let isGameOver = false;
+
+reset();
+
+// functions
+function reset() {
+  currentPlayer = 1;
+  currentScore = 0;
+  mainScore1 = 0;
+  mainScore2 = 0;
+  isGameOver = false;
+
+  if (side2UI.classList.contains("side--winner"))
+    side2UI.classList.remove("side--winner");
+
+  if (side1UI.classList.contains("side--winner"))
+    side1UI.classList.remove("side--winner");
+
+  if (side2UI.classList.contains("side--active")) {
+    side2UI.classList.remove("side--active");
+  }
+
+  if (!side1UI.classList.contains("side--active")) {
+    side1UI.classList.add("side--active");
+  }
+
+  scoreMain1UI.textContent = scoreMain2UI.textContent = 0;
+
+  currentScore1UI.textContent = currentScore2UI.textContent = 0;
+
+  if (!diceUI.classList.contains("hidden")) diceUI.classList.add("hidden");
+}
+
+const changeActiveSide = function () {
+  side1UI.classList.toggle("side--active");
+  side2UI.classList.toggle("side--active");
+};
+
+const switchUser = function () {
+  const scoreUI = getCompnentByCurrentPlayer(currentScore1UI, currentScore2UI);
+  scoreUI.textContent = currentScore = 0;
+  if (currentPlayer === 1) currentPlayer = 2;
+  else currentPlayer = 1;
+  changeActiveSide();
+};
+
+const forScore = function (number) {
+  const scoreUI = getCompnentByCurrentPlayer(currentScore1UI, currentScore2UI);
+
+  if (number === 1) {
+    switchUser();
+    return;
+  }
+  scoreUI.textContent = currentScore += number;
+};
+
+const checkWinner = function () {
+  const mainScore = getCompnentByCurrentPlayer(mainScore1, mainScore2);
+  if (mainScore >= 100) {
+    const side = getCompnentByCurrentPlayer(side1UI, side2UI);
+    side.classList.add("side--winner");
+    // changing color theme when game is over
+    root.style.setProperty("--color-theme-1", "#DF2935");
+    root.style.setProperty("--color-theme-2", "#C94E4C");
+    isGameOver = true;
+  }
+};
+
+// globel function
+function getCompnentByCurrentPlayer(comp1, comp2) {
+  return currentPlayer === 1 ? comp1 : comp2;
+}
+
+// when pressing roll button
+btnRoll.addEventListener("click", function (event) {
+  if (!isGameOver) {
+    event.preventDefault();
+    const randomNumber = Math.trunc(Math.random() * 6) + 1;
+
+    diceUI.classList.remove("hidden");
+    diceUI.src = `dice-${randomNumber}.png`;
+
+    forScore(randomNumber);
+  }
+});
+
+// when pressing hold button
+btnHold.addEventListener("click", function (event) {
+  if (!isGameOver) {
+    event.preventDefault();
+    if (currentPlayer === 1)
+      scoreMain1UI.textContent = mainScore1 += currentScore;
+    else scoreMain2UI.textContent = mainScore2 += currentScore;
+    checkWinner();
+    switchUser();
+  }
+});
+
+// when pressing new game button
+btnReset.addEventListener("click", reset);
